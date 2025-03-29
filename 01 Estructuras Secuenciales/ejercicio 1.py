a5d343b266fa30138ca3d0950726da97762d8a29
Tevan88 <tevanrivarola@gmail.com>
2025-03-29 16:48:29 -0300

    nuevos comentarios

--- 01 Estructuras Secuenciales/ejercicio 1.py
+++ 01 Estructuras Secuenciales/ejercicio 1.py
@@ -2,5 +2,8 @@
 #el nombre ingresado. Por ejemplo: si el usuario ingresa “Marcos”, el programa debe imprimir
 #por pantalla “Hola Marcos!”. Consejo: esto será más sencillo si utilizas print(f…) para
 #realizar la impresión por pantalla.
+
+#Definimos variable nombre
 nombre = input("Ingrese su nombre: ")
+#Mostramos mensaje por pantalla
 print(f"Hola {nombre}!")
