679df35cb647034f592927103bbebabf074b6381
Tevan88 <tevanrivarola@gmail.com>
2025-03-31 20:10:56 -0300

    Agregue comentarios a ejercicio2

--- 03 Estructuras Condicionales/ejercicio2.py
+++ 03 Estructuras Condicionales/ejercicio2.py
@@ -2,9 +2,12 @@
 #mostrar por pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar el
 #mensaje “Desaprobado”.
 
+#Le pedimos la nota al usuario definiendo la variable nota.
 nota = float(input("Ingrese su calificacion (del 1 al 10):"))
-
+#Comenzamos el condicional if la nota ingresada es mayor o igual a 6.
 if nota >= 6:
+    #Imprimimos el mensaje aprobado por pantalla.
     print("Aprobado")
 else:
+    #Sino mostramos el mensaje desaprobado por pantalla.
     print("Desaprobado")
