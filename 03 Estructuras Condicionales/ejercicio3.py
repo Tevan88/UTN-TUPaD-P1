929e8f2d9670cd447a4dec120d117e1a062a9887
Tevan88 <tevanrivarola@gmail.com>
2025-03-31 20:15:41 -0300

    Agregue comentarios a ejercicio3

--- 03 Estructuras Condicionales/ejercicio3.py
+++ 03 Estructuras Condicionales/ejercicio3.py
@@ -3,9 +3,12 @@
 #contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del
 #operador de módulo (%) en Python para evaluar si un número es par o impar.
 
+#Definimos la variable num pidiendole un numero entero positivo al usuario.
 num = int(input("Ingrese un número entero positivo:"))
-
+#Comenzamos el condicional if con dos condiciones, si num es mayor o igual a 0 Y si num dividido 2 el resto es 0.
 if (num >= 0) and (num % 2 == 0):
+    #Mostramos el mensaje en pantalla.
     print("Ha ingresado un número par")
 else:
+    #Mostramos el mensaje por pantalla si no se cumple la condicion.
     print("Por favor, ingrese un número par")
