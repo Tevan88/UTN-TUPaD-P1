521792107f59a4a02dbbcf8595d39d0bbe6ebd84
Tevan88 <tevanrivarola@gmail.com>
2025-03-31 21:06:54 -0300

    Falta terminar este ejercicio, no me da la longitud, reveer esto

---
+++ 03 Estructuras Condicionales/ejercicio5.py
@@ -0,0 +1,16 @@
+#Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres
+#(incluyendo 8 y 14). Si el usuario ingresa una contraseña de longitud adecuada, imprimir por en
+#pantalla el mensaje "Ha ingresado una contraseña correcta"; en caso contrario, imprimir por
+#pantalla "Por favor, ingrese una contraseña de entre 8 y 14 caracteres". Nota: investigue el uso
+#de la función len() en Python para evaluar la cantidad de elementos que tiene un iterable tal
+#como una lista o un string.
+
+#Definimos la variable contraseña con un input para que el usuario ingrese una serie de numeros (entre 8 y 14 caracteres)
+contra = input("Ingrese su contraseña entre 8 y 14 caracteres:")
+#Definimos la variable longitud como el largo de la cadena de la contraseña.
+longitud = len(contra)
+
+if longitud >= 8 and longitud <= 14:
+    print("Ha ingresado una contraseña correcta")
+else:
+    print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
