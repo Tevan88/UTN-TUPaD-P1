40b4f04e8b3b56ef9ca8f222ac9a8412ed906cc5
Tevan88 <tevanrivarola@gmail.com>
2025-04-07 18:46:59 -0300

    Creamos el archivo como prueba para tener de referencia

--- 04 Estructuras Repetitivas/ejercicio1.py
+++
@@ -1,4 +0,0 @@
-x = 1
-
-for x in range(11,1,-2):
-    print(x, "Debo practicar ciclos FOR")
---
+++ 04 Estructuras Repetitivas/pruebas.py
@@ -0,0 +1,14 @@
+for x in range(11,1,-2):
+    print(x, "Debo practicar ciclos FOR")
+
+print("////////")
+
+x = 1
+while x <= 5:
+    print(x,"Debo aprender ciclos")
+    x = x + 1
+
+x = 5
+while x >= 1:
+    print(x,"Debo aprender ciclos")
+    x = x - 1
