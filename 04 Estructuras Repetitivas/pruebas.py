97d288bdca6d742a2f2eb36b5e65ee963447204a
Tevan88 <tevanrivarola@gmail.com>
2025-04-07 19:03:35 -0300

    Le agregamos por bandera

--- 04 Estructuras Repetitivas/pruebas.py
+++ 04 Estructuras Repetitivas/pruebas.py
@@ -7,8 +7,18 @@ x = 1
 while x <= 5:
     print(x,"Debo aprender ciclos")
     x = x + 1
-
+print("////////")
 x = 5
 while x >= 1:
     print(x,"Debo aprender ciclos")
     x = x - 1
+
+print("//// Por bandera ////")
+
+umbral = 10
+sumatoria = 0
+
+while sumatoria < umbral:
+    print("Ingrese un número: ")
+    num = int(input())
+    sumatoria += num # Esto es igual a sumatoria = sumatoria + num
