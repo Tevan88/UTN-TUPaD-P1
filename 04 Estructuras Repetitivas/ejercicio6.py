db7f9168fa7852e07494bc291d5fee0ddf6ce40c
Tevan88 <tevanrivarola@gmail.com>
2025-04-08 20:09:43 -0300

    Listo ejercicio6

---
+++ 04 Estructuras Repetitivas/ejercicio6.py
@@ -0,0 +1,6 @@
+#Desarrolla un programa que imprima en pantalla todos los números pares comprendidos
+#entre 0 y 100, en orden decreciente.
+
+#Realizamos un bucle for que comience en 100, termine en -1 (para que incluya al 0) y con paso -2 (para mostrarlo en forma decreciente y los pares)
+for x in range(100,-1,-2):
+    print(x) #Vamos mostrando en cada bucle los numeros por pantalla
