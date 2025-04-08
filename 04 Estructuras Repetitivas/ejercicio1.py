fe4781ef30f5374385081bfacea668f080b96e34
Tevan88 <tevanrivarola@gmail.com>
2025-04-08 19:01:58 -0300

    ejercicio1 listo

---
+++ 04 Estructuras Repetitivas/ejercicio1.py
@@ -0,0 +1,7 @@
+#Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100
+#(incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
+
+#Comenzamos el bucle for que va desde el rango 0 (valor por defecto de la variable de inicio) al 101 (ya que no incluye el valor final)
+for x in range(101):
+    print(x) #Mostramos por pantalla el valor de x cada vez que realiza el bucle
+
