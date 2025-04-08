6500ab32c3ed267b57a942372c651cda334b7323
Tevan88 <tevanrivarola@gmail.com>
2025-04-08 16:25:17 -0300

    Agregue sumatorios y promedios

--- 04 Estructuras Repetitivas/pruebas.py
+++ 04 Estructuras Repetitivas/pruebas.py
@@ -22,3 +22,20 @@ while sumatoria < umbral:
     print("Ingrese un número: ")
     num = int(input())
     sumatoria += num # Esto es igual a sumatoria = sumatoria + num
+
+print("/////Sumatorias y promedios/////")
+
+cant_numeros = 5
+sumatoria = 0
+
+for cont in range(1,cant_numeros+1): #Le ponemos a cant_numeros+1 así incluye el 5 como valor final
+    print("Ingrese un número",cont)
+    num = int(input())
+    sumatoria = sumatoria + num
+    print()
+
+print("La sumatoria de los numeros ingresados es: ", sumatoria)
+print("El promedio entre los numeros ingresados es: ",(sumatoria/cant_numeros))
+
+
+
