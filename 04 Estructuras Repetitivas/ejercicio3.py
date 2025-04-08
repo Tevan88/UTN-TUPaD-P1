0f6e97ff7ec2fb1a13db533754e0394f5f61ed99
Tevan88 <tevanrivarola@gmail.com>
2025-04-08 19:34:57 -0300

    Falta terminar este ejercicio, seguir luego

---
+++ 04 Estructuras Repetitivas/ejercicio3.py
@@ -0,0 +1,15 @@
+#Escribe un programa que sume todos los números enteros comprendidos entre dos valores
+#dados por el usuario, excluyendo esos dos valores.
+
+#Le pedimos por consola dos numeros enteros al usuario.
+num_min = int(input("Ingrese un número entero: "))
+num_max = int(input("Ingrese un número entero mayor al anterior: "))
+x = num_min
+
+if num_min < num_max:
+    for cont in range(num_min+1, num_max):
+        x += 1
+else:
+    print("El segundo número debe ser mayor al primero ingresado")
+
+print(f"La suma de todos los numeros enteros comprendidos entre {num_min} y {num_max} es: {x}")
