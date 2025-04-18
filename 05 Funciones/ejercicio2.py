571e9275642e7ba4d866c15519bc4942c66e77fa
Tevan88 <tevanrivarola@gmail.com>
2025-04-18 18:23:32 -0300

    Listo el 2

--- 05 Funciones/ejercicio2.py
+++ 05 Funciones/ejercicio2.py
@@ -3,4 +3,13 @@
 #Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá devolver: “Hola Marcos!”. 
 # Llamar a esta función desde el programa principal solicitando el nombre al usuario.
 
+
 def saludar_usuario(nombre):
+    return nombre
+
+#Programa principal.
+
+#Definimos la variable nombre con lo que ingrese el usuario como su nombre
+nombre = input("Escriba su nombre: ")
+#Mostramos por pantalla 
+print(saludar_usuario(f"Hola {nombre}!"))
