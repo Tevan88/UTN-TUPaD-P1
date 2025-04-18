e3d9ebef4c731df62d3c08f45f5b85322718278d
Tevan88 <tevanrivarola@gmail.com>
2025-04-18 19:00:29 -0300

    Listo el 4

--- 05 Funciones/ejercicio4.py
+++ 05 Funciones/ejercicio4.py
@@ -2,7 +2,7 @@
 # y devuelva el área del círculo. calcular_perimetro_circulo(radio) que reciba el radio 
 # como parámetro y devuelva el perímetro del círculo. Solicitar el radio al usuario 
 # y llamar ambas funciones para mostrar los resultados.
-PI = 3,1416
+PI = 3.1416
 def calcular_area_circulo(radio):
     return PI * radio ** 2
 
@@ -15,5 +15,5 @@ radio = float(input("Ingrese el radio del círculo: "))
 area = calcular_area_circulo(radio)
 perimetro = calcular_perimetro_circulo(radio)
 
-print(f"El area del círculo es: {area}")
-print(f"El perímetro del círculo es: {perimetro}")
+print(f"El area del círculo es: {area:.2f}")
+print(f"El perímetro del círculo es: {perimetro:.2f}")
