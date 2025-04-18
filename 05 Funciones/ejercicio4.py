758d661f9dd8600e437ef0f029a9b5a57897dba4
Tevan88 <tevanrivarola@gmail.com>
2025-04-18 18:58:16 -0300

    Listo el 4

---
+++ 05 Funciones/ejercicio4.py
@@ -0,0 +1,19 @@
+#Crear dos funciones: calcular_area_circulo(radio) que reciba el radio como parámetro 
+# y devuelva el área del círculo. calcular_perimetro_circulo(radio) que reciba el radio 
+# como parámetro y devuelva el perímetro del círculo. Solicitar el radio al usuario 
+# y llamar ambas funciones para mostrar los resultados.
+PI = 3,1416
+def calcular_area_circulo(radio):
+    return PI * radio ** 2
+
+def calcular_perimetro_circulo(radio):
+    return 2 * PI * radio
+
+#Programa principal
+
+radio = float(input("Ingrese el radio del círculo: "))
+area = calcular_area_circulo(radio)
+perimetro = calcular_perimetro_circulo(radio)
+
+print(f"El area del círculo es: {area}")
+print(f"El perímetro del círculo es: {perimetro}")
--- 05 Funciones/funciones.py
+++ 05 Funciones/funciones.py
@@ -1,16 +1,4 @@
-#Definicion de nuevas funciones
-def obtener_resto(num1, num2):
-    return num1 % num2
+def saludo(nombre):
+    return f"Hola, {nombre}!"
 
-def es_multiplo(x, y):
-    return obtener_resto(x, y) == 0
-
-#Programa principal
-a = int(input("Primer número: "))
-b = int(input("Segundo número: "))
-
-resto = obtener_resto(a, b)
-
-print(f"El resto entre {a} y {b} es {resto}")
-if es_multiplo(a, b):
-    print(f"{a} es múltiplo de {b}")
+print(saludo("Ana"))
