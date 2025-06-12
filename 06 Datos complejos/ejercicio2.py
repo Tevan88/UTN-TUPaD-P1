caae09f099b69da87f7c248c06c003a6ce0a2822
Tevan88 <tevanrivarola@gmail.com>
2025-06-12 19:40:41 -0300

    Listo el 2

--- 06 Datos complejos/ejercicio2.py
+++ 06 Datos complejos/ejercicio2.py
@@ -6,4 +6,10 @@
 
 
 precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':1450, 'Naranja': 1200, 'Manzana': 1500, 'Pera': 1200}
+print(precios_frutas)
 
+precios_frutas['Banana'] = 1330
+precios_frutas['Manzana'] = 1700
+precios_frutas['Melón'] = 2800
+
+print(precios_frutas)
