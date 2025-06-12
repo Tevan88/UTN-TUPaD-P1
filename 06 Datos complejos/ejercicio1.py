be8ba6f3206b2ec736849b2030e347b41feeaee1
Tevan88 <tevanrivarola@gmail.com>
2025-06-12 19:39:07 -0300

    Listo el 1

--- 06 Datos complejos/ejercicio1.py
+++ 06 Datos complejos/ejercicio1.py
@@ -1,6 +1,5 @@
 #Dado el diccionario precios_frutas:
-precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':
-1450}
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
 #Añadir las siguientes frutas con sus respectivos precios:
 # Naranja = 1200
 # Manzana = 1500
---
+++ 06 Datos complejos/ejercicio2.py
@@ -0,0 +1,9 @@
+#Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código
+#desarrollado en el punto anterior, actualizar los precios de las siguientes frutas:
+# Banana = 1330
+# Manzana = 1700
+# Melón = 2800
+
+
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':1450, 'Naranja': 1200, 'Manzana': 1500, 'Pera': 1200}
+
